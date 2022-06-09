3398d34f2842b7d343e8d02af3d064207a20ab82
Mahmut Atilay <mahmudatilay@gmail.com>
2022-06-09 14:00:35 +0300

    part2

---
+++ custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function()
+{
+
+$("#div_msg").append('<div id="divx" class="chat-message-right mb-4"> </div>')
+$("#divx").append('<div id="divy" class="flex-shrink-1 bg-light rounded py-2 px-3 mr-3"> Merhaba nasılsın ben iyim </div> ')
+$("#divy").append('<div class="font-weight-bold mb-1"> You </div>')
+
+
+
+});
+
