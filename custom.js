d4deb877746d0c90952c93b7816836d98fd6c988
Mahmut Atilay <mahmudatilay@gmail.com>
2022-06-11 00:57:37 +0300

    json_jquary_js_basic_message

--- custom.js
+++ custom.js
@@ -1,33 +1,73 @@
 $(document).ready(function()
 {
-    let data={
-            kisiler:[
-                {name:"Mahmud",mesaj:"hello Nasılsınız"},
-                {name:"İsmail",Mesaj:"how Are you"}
-            ]
-    }
+var obj = [];
+ 
 
-    
-    
-
-   var msgVeri= data.kisiler
-   
-  
-
-for(i=0;i<msgVeri.length;i++)
+$("#btn_id").click(function(){addMessage()})
+$("#btn_uadd").click(function(){Useradd()})
+var x=0
+var active_tab=0;
+function Useradd()
 {
-    
-    
-    $("#div_msg").append('<div id="divx" class="chat-message-right mb-4"><div name="divy" class="flex-shrink-1 bg-light rounded py-2 px-3 mr-3"></div></div>')
 
-  $(".divy").html("hello")
-    
+  tmp = {
+    'id':x,
+    'name': $("#user_id").val(),
+    'message': []
+};
+      obj.push(tmp);
+
    
 
+    var userDiv= document.createElement("a")
+    userDiv.setAttribute("class","list-group-item list-group-item-action border-0")
+    
+    var userDiv1=document.createElement("div")
+    userDiv1.setAttribute("class","d-flex align-items-start") 
+    userDiv1.value=obj[x].id
+    userDiv1.onclick=function() 
+    {active_tab=userDiv1.value ,list()};
+    userDiv1.innerText=$("#user_id").val()
+    userDiv.appendChild(userDiv1)
+    
+    document.getElementById("us_div").appendChild(userDiv);
+    
+    x++
+    console.log(obj)
 
 
 }
-alert($(".divy").html())
+
+function addMessage(){
+
+  obj[active_tab].message.push($("#msj_txt").val());
+ list()
+}
+
+function list(){
+
+  document.getElementById("div_msg").innerHTML=""
+  for(i=0;i< obj[active_tab].message.length; i++)
+{
+
+  var dOne= document.createElement("div")
+  dOne.setAttribute("class","chat-message-right mb-4")
+  var dTwo=document.createElement("div")
+  dTwo.setAttribute("class","flex-shrink-1 bg-light rounded py-2 px-3 mr-3")
+  dTwo.innerText=obj[active_tab].message[i]
+  var dTh=document.createElement("div")
+  dTh.setAttribute("class","font-weight-bold mb-1")
+  dTh.innerText=""
+  dTwo.appendChild(dTh)
+  dOne.appendChild(dTwo)
+  
+ document.getElementById("div_msg").appendChild(dOne);
+
+
+}
+  
+}
+
 
 
 });
